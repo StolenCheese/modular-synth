9728fb307d5db5497d954de99c534dccd484a546
gads2 <gwendolen@dennis-sellers.com>
2023-02-07 17:19:52 +0000

    Added a basic mouse input handler class and messed with having a COMPLETELY RANDOM image appear on screen

---
+++ modular-synth-frontend/Scripts/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace modular_synth_frontend.Scripts;
+
+    public class InputManager
+{
+    MouseState oldState, newState;
+
+    public void Update()
+    {
+        newState = Mouse.GetState();
+        oldState = newState;
+    }
+
+    public bool LeftMousePressed()
+    {
+        if(newState.LeftButton == ButtonState.Pressed)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool LeftMouseClickDown()
+    {
+        if (newState.LeftButton == ButtonState.Pressed && oldState.LeftButton == ButtonState.Released)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool LeftMouseClickUp()
+    {
+        if(newState.LeftButton == ButtonState.Released && oldState.LeftButton == ButtonState.Pressed)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+

34472e963d7a34f445474dc9a212b8e3a68e7efe
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-02-10 20:52:27 +0000

    dll making and running from frontend
    Creation of visual studio c++ .dll making project.
    Importing .dll file into c# function
    Running c# function from Program

--- modular-synth-frontend/Scripts/Program.cs
+++ modular-synth-frontend/Scripts/Program.cs
@@ -1,5 +1,5 @@
-﻿//using System;
-//using static modular_synth_frontend.Scripts.BackendComms;
-//Console.WriteLine(addNumbers(1,2));
+﻿using System;
+using static modular_synth_frontend.BackendComms.BackendComm;
+Console.WriteLine(addNumbers(1,2));
 using var game = new modular_synth_frontend.Scripts.Game1();
 game.Run();

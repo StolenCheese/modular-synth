c60bff776a48e657f8bcfd17e5607ba027085b0b
Marmaladesparrow12 <gwendolen@dennis-sellers.com>
2023-02-13 12:22:17 +0000

    Updated Module

--- modular-synth-frontend/Core/Module.cs
+++ modular-synth-frontend/Core/Module.cs
@@ -1,7 +1,10 @@
 namespace modular_synth_frontend.Core;
 public class Module : Entity{
+    Transform t = new Transform();
+    SpriteRenderer sprite = new SpriteRenderer();
+
     public Module(){
-        base.AddComponent(new Transform());
-        base.AddComponent(new SpriteRenderer());
+        base.AddComponent(t);
+        base.AddComponent(sprite);
     }
 }

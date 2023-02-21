fdb8971015eb2857a3aa2156c0b9ba0471c2c146
WillHustler <wrch3@cam.ac.uk>
2023-02-21 16:26:37 +0000

    Added Combining UI and Sec Def files

---
+++ modular-synth-frontend/SectionDef/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+namespace SectionDefTest
+{
+    internal class Program
+    {
+        //input UIDef file path, SecDefFile path and CombinedFile as an path
+        static public void combineSecUIDef(string UIDefFile, string SecDefFile, string CombinedFile) 
+        {
+            //read in UIDef and SecDef Files
+            string jsonUI = File.ReadAllText(UIDefFile);
+            Dictionary<string, Dictionary<string, string>> UIDefValues = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(jsonUI);
+            string jsonSec = File.ReadAllText(SecDefFile);
+            Dictionary<string, Dictionary<string, string>> SecDefValues = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(jsonSec);
+
+            //check for each component in SecDef there is a matching component in UIDef
+            foreach (var component in SecDefValues)
+            {
+                if (!UIDefValues.ContainsKey(component.Key))
+                {
+                    throw new InvalidOperationException("SecDef file contains parameters not found in UIDef File");
+                }
+            }
+
+            Dictionary<string, Dictionary<string, string>> combinedFile = new Dictionary<string, Dictionary<string, string>>();
+            foreach (var component in SecDefValues) //loop over SecDef as we do not mind if there are additional things in UIDef that we don't use
+            {
+                Dictionary<string, string> newDict = new Dictionary<string, string>();
+                //add parameters in both UI and Se Def for that component
+                newDict = UIDefValues[component.Key];
+                foreach (var a in component.Value)
+                {
+                    newDict.Add(a.Key, a.Value);
+                }
+
+                combinedFile.Add(component.Key, newDict);
+            }
+            string json = JsonConvert.SerializeObject(combinedFile, Formatting.Indented);
+            File.WriteAllText(CombinedFile, json);
+        }
+    }
+    
+}

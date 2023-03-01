cb9ea127ac18d600eb6f4d189ad759755b628712
Manby <kofiwii@googlemail.com>
2023-03-01 13:07:17 +0000

    Code for node reordering (does not compile yet)

--- modular-synth-backend/src/synth-api/section/PortManager.cpp
+++ modular-synth-backend/src/synth-api/section/PortManager.cpp
@@ -5,9 +5,11 @@
 #include "synth-api/section/_model/PortManager.h"
 #include "synth-api/section/Section.h"
 #include "sc-controller/SetterFunctor.h"
+#include "sc-controller/SuperColliderController.hpp"
 
 #include <unordered_map>
 #include <list>
+#include <vector>
 
 namespace synth_api {
     std::unordered_map<Port*, Section*> PortManager::parentMap = std::unordered_map<Port*, Section*>();
@@ -27,7 +29,7 @@ namespace synth_api {
     void PortManager::reorder(OutputPort *root) {
         std::unordered_map<Section *, Stage> stages;
         std::list<Section *> stack;
-        std::list<Section *> order;
+        std::vector<int> order;
 
         Section *parent = parentMap[root];
         stack.insert(stack.begin(), parent);
@@ -52,10 +54,12 @@ namespace synth_api {
 
                 case Explored:  // add to order
                     stack.pop_back();
-                    order.insert(order.end(), curr);
+                    order.push_back(curr->synth->index);
                     break;
 }
         }
         // SCOOP server.pushNodesToStartOfEvalOrder(order); // i.e. the /n_order command with add action 0, use root group
+        SuperColliderController controller = SuperColliderController::get();
+        controller.n_order(0, 0, order);
     }
 }

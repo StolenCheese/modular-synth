971a5f95863e9095b76a521944fcf8d47aed969f
WillHustler <wrch3@cam.ac.uk>
2023-03-04 20:27:28 +0000

    added basic module creation

--- modular-synth-frontend/UI/Module.cs
+++ modular-synth-frontend/UI/Module.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using SynthAPI;
 using modular_synth_frontend.API;
 namespace modular_synth_frontend.UI;
+using Newtonsoft.Json;
+using System.IO;
 public class Module : Interactable
 {
     private InputManager input = InputManager.GetInstance();
@@ -68,7 +70,112 @@ public class Module : Interactable
         API.API.createSection(this);
         sendInitialComponentValsToServer();
     }
+    public Module(Texture2D sprite, Vector2 pos, string uiSecDefFile, GraphicsDevice graphicsDevice) : base(sprite, pos)
+    {
+        this.ModuleId = modules++;
+        //this is in SectionDefFile but I can't seem to import it for some reason
+        string jsonCombinedFile = File.ReadAllText(uiSecDefFile);
+        Dictionary<string, Dictionary<string, string>> UISecDefDict = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(jsonCombinedFile);
+        foreach (KeyValuePair<string, Dictionary<string, string>> component in UISecDefDict)
+        {
+            Dictionary<string, string> newComp = component.Value;
+            if (newComp.TryGetValue("componentType", out string ComponentType))
+            {
+                if (ComponentType == "slider")
+                {
+                    string xPosString = newComp.TryGetValue("xPos", out string compXPos) ? compXPos : "error";                            //get xPosition
+                    string yPosString = newComp.TryGetValue("yPos", out string compYPos) ? compYPos : "error";                            //get yPosition            
+                    string colString = newComp.TryGetValue("col", out string compCol) ? compCol : "error";                                //get Colour
+                    string parameterIDString = newComp.TryGetValue("parameterID", out string compParameterID) ? compParameterID : "error";//get ParameterID
+                    string trackScaleString = newComp.TryGetValue("trackScale", out string compTrackScale) ? compTrackScale : "1";      //get trackScale, if not default to 1
+                    string sliderScaleString = newComp.TryGetValue("sliderScale", out string compSliderScale) ? compSliderScale : "1";   //get trackScale, if not default to 1
+                    string isVerticalString = newComp.TryGetValue("isVertical", out string compIsVertical) ? compIsVertical : "false";  //get trackScale, if not default to false
+                    string trackSpriteString = newComp.TryGetValue("trackSprite", out string compTrackSprite) ? compTrackSprite : "error";
+                    string sliderSpriteString = newComp.TryGetValue("sliderSprite", out string compSliderSprite) ? compSliderSprite : "error";
 
+                    if (xPosString == "error" || yPosString == "error" || colString == "error" || parameterIDString == "error" || sliderSpriteString == "error" || trackSpriteString == "error")
+                    {
+                        Console.WriteLine("error making slider");
+                        //TODO: Handle error
+                    }
+
+                    Vector2 moduleLocalPos = new Vector2(int.Parse(xPosString), int.Parse(yPosString));    //combine, xypos into vector
+                    Color col = (Color)new System.Drawing.ColorConverter().ConvertFromString(colString);   //convert colour to colour type
+                    double trackScale = double.Parse(trackScaleString);
+                    double sliderScale = double.Parse(sliderScaleString);
+                    bool isVertical = bool.Parse(isVerticalString);
+                    Texture2D trackSprite = Texture2D.FromFile(graphicsDevice, trackSpriteString);
+                    Texture2D sliderSprite = Texture2D.FromFile(graphicsDevice, sliderSpriteString);
+
+                    components.Add(new Slider(pos, ModuleId, moduleLocalPos, trackSprite, sliderSprite, col, parameterIDString, trackScale, sliderScale, isVertical));
+                }
+                else if (ComponentType == "dial")
+                {
+                    string xPosString = newComp.TryGetValue("xPos", out string compXPos) ? compXPos : "error";                                            //get xPosition
+                    string yPosString = newComp.TryGetValue("yPos", out string compYPos) ? compYPos : "error";                                            //get yPosition
+                    string colString = newComp.TryGetValue("col", out string compCol) ? compCol : "error";                                                //get Colour
+                    string parameterIDString = newComp.TryGetValue("parameterID", out string compParameterID) ? compParameterID : "error";                //get ParameterID
+                    string staticPartScaleString = newComp.TryGetValue("staticPartScale", out string compstaticPartScale) ? compstaticPartScale : "1";//get staticPartScale
+                    string dialScaleString = newComp.TryGetValue("dialScale", out string compdialScale) ? compdialScale : "1";                        //get dialScale
+                    string staticPartSpriteString = newComp.TryGetValue("staticPartSprite", out string compStaticPartSprite) ? compStaticPartSprite : "error";
+                    string dialSpriteString = newComp.TryGetValue("dialSprite", out string compDialSprite) ? compDialSprite : "error";
+
+                    if (xPosString == "error" || yPosString == "error" || colString == "error" || parameterIDString == "error" || dialSpriteString == "error" || staticPartSpriteString == "error")
+                    {
+                        Console.WriteLine("error making dial");
+                        //TODO: Handle error
+                    }
+
+                    Vector2 moduleLocalPos = new Vector2(int.Parse(xPosString), int.Parse(yPosString));    //combine, xypos into vector
+                    Color col = (Color)new System.Drawing.ColorConverter().ConvertFromString(colString);   //convert colour to colour type
+                    double staticPartScale = double.Parse(staticPartScaleString);
+                    double dialScale = double.Parse(dialScaleString);
+                    Texture2D staticPartSprite = Texture2D.FromFile(graphicsDevice, staticPartSpriteString);
+                    Texture2D dialSprite = Texture2D.FromFile(graphicsDevice, dialSpriteString);
+
+                    components.Add(new Dial(pos, ModuleId, moduleLocalPos, staticPartSprite, dialSprite, col, parameterIDString, staticPartScale, dialScale));
+                }
+                else if (ComponentType == "port")
+                {
+                    string xPosString = newComp.TryGetValue("xPos", out string compXPos) ? compXPos : "error";                                            //get xPosition
+                    string yPosString = newComp.TryGetValue("yPos", out string compYPos) ? compYPos : "error";                                            //get yPosition
+                    string colString = newComp.TryGetValue("col", out string compCol) ? compCol : "error";                                                //get Colour
+                    string parameterIDString = newComp.TryGetValue("parameterID", out string compParameterID) ? compParameterID : "error";                //get ParameterID
+                    string isInputString = newComp.TryGetValue("isInput", out string compIsInput) ? compIsInput : "error";                                //get isInput
+                    string scaleString = newComp.TryGetValue("scale", out string compScale) ? compScale : "1";                                            //get Scale
+                    string spriteString = newComp.TryGetValue("sprite", out string compSprite) ? compSprite : "error";
+
+                    if (xPosString == "error" || yPosString == "error" || colString == "error" || parameterIDString == "error" || spriteString == "error")
+                    {
+                        Console.WriteLine("error making port");
+                        //TODO: Handle error
+                    }
+
+                    Vector2 moduleLocalPos = new Vector2(int.Parse(xPosString), int.Parse(yPosString));    //combine, xypos into vector
+                    Color col = (Color)new System.Drawing.ColorConverter().ConvertFromString(colString);   //convert colour to colour type
+                    double scale = double.Parse(scaleString);
+                    Texture2D spritePort = Texture2D.FromFile(graphicsDevice, spriteString);
+                    bool isInput = bool.Parse(isInputString);
+
+                    components.Add(new Port(pos, ModuleId, moduleLocalPos, spritePort, col, parameterIDString, isInput, scale));
+                }
+                else if (ComponentType == "button")
+                {
+                    //TODO: add button creation
+                }
+            }
+            else
+            {
+                //Error, do not know component type
+            }
+        }
+
+
+        addToEtyMgr();
+
+        API.API.createSection(this);
+        sendInitialComponentValsToServer();
+    }
 
     public void addToEtyMgr(){
         EntityManager.entities.Add(this);

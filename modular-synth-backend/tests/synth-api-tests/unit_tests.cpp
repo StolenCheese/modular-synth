ff2008b1e3cc2708aa65aa22838026189100b12d
Jamie (Aetherflux Reservoir) <bmsyiek@gmail.com>
2023-03-05 15:38:31 +0000

    test now compiles and run however i am getting an error in servercommander.lib

--- modular-synth-backend/tests/synth-api-tests/unit_tests.cpp
+++ modular-synth-backend/tests/synth-api-tests/unit_tests.cpp
@@ -1,16 +1,17 @@
 #include <doctest/doctest.h>
 
-#include "synth-api/section/Section.h"
+#include "synth-api/section/Section.h""
 #include "synth-api/exception/LinkException.hpp"
+#include "sc-controller/SuperColliderController.hpp"
 
 //#define SYNTH_API_ROOT_FOLDER @SYNTHDEFS_FILE_LOCATION@
-#define SYNTH_API_ROOT_FOLDER "D:REPOS\\modular-synth\\modular-synth-backend\\synthdefs"
+#define SYNTH_API_ROOT_FOLDER "D:\\REPOS\\modular-synth\\modular-synth-backend\\synthdefs"
 
 TEST_CASE("We can link I/P to I/P on the same section, but not I/P to O/P on the same section") {
 	synth_api::Section section = synth_api::Section(SYNTH_API_ROOT_FOLDER "\\sin-ar.scsyndef", "");
 
 	SUBCASE("Linking I/P to I/P on same section is allowed") {
-		CHECK_NOTHROW(section.getPortFor("freq")->linkTo(section.getPortFor("add")));
+		CHECK_NOTHROW(section.getPortFor("mul")->linkTo(section.getPortFor("add")));
 	}
 
 	SUBCASE("Linking I/P to O/P on same section throws an exception") {
@@ -18,5 +19,18 @@ TEST_CASE("We can link I/P to I/P on the same section, but not I/P to O/P on the
 			section.getPortFor("freq")->linkTo(section.getPortFor("out")), 
 			synth_api::CyclicLinksException
 		);
+	}
+}
+
+int main(int argc, char** argv) {
+	auto endpoint = IpEndpointName("127.0.0.1", 58000);
+	SuperColliderController::Connect(endpoint);
+	std::cout << "Ensure you have the SC Server started\n";
+	doctest::Context context;
+	context.applyCommandLine(argc, argv);
+	int res = context.run();
+
+	if (context.shouldExit()) {
+		return res;
 	}
 }
